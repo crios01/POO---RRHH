3a36f0734ad7ef2a08d3075efd04376af61fb213
jcvasquez <u201200694@upc.edu.pe>
2012-03-29 16:50:51 -0500

    form

--- ProyectoFinal/src/Vistas/Login.java
+++ ProyectoFinal/src/Vistas/Login.java
@@ -32,11 +32,11 @@ public class Login extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 627, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 201, Short.MAX_VALUE)
         );
 
         pack();
